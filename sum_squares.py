f1a8cde6d02a45335f860459f35b4f0c3dc82f93
AshleyMDickson <ashley.dickson.23@ucl.ac.uk>
2024-11-24 16:01:09 +0000

    Fix primality test

--- sum_squares.py
+++ sum_squares.py
@@ -1,25 +1,22 @@
-#import prime_test as pt
+import prime_test as pt
 
 def test_sum_squares(x):
-
     #print(f'{x}:')
     square_numbers = []
 
     for i in range(1, int(x**(1/2))+1):
         square_numbers.append(i**2)
-
     #print(square_numbers)
-    #if pt.prime_test_func(x) == 1:
+    if pt.prime_test_func(x) == 1:
     
-    for sq in square_numbers:
-        y = x - sq
-        #print(y)
-        if y in square_numbers:
-            print(f'{x} = {y} + {sq}')
-            square_numbers.remove(sq)
-
-        else:
-            pass
-    #else:
-    #    pass
+        for sq in square_numbers:
+            y = x - sq
+            #print(y)
+            if y in square_numbers:
+                print(f'{x} = {y} + {sq}')
+                square_numbers.remove(sq)
+            else:
+                pass
+    else:
+        pass
 #test_sum_squares(97)
