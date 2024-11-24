34e93d362832683af2711053a3b785bdd1858d91
AshleyMDickson <ashley.dickson.23@ucl.ac.uk>
2024-11-24 16:01:14 +0000

    Updating branch structure

--- prime_test.py
+++ prime_test.py
@@ -4,19 +4,17 @@ def prime_test_func(x):
     
     factors = []
     
-    for i in range(2, int(x**(1/2))):
-        
+    for i in range(2, int(x**(1/2))+1):
         if x%i == 0:
-            
             factors.append(i)
             
     y = len(factors)
     
     if y==0:
-        return 1;
         #print(f'{x} is prime.')
+        return 1;
     else:
-        return 0;
         #print(f'{x} is composite.')
+        return 0;
 
 #prime_test_func(a)
