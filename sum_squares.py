fe19c04892ea0246a81ef877b0aff8c997fb0db2
AshleyMDickson <ashley.dickson.23@ucl.ac.uk>
2024-11-25 14:01:41 +0000

    Prime testing logic update

--- sum_squares.py
+++ sum_squares.py
@@ -7,13 +7,17 @@ def test_sum_squares(x):
     for i in range(1, int(x**(1/2))+1):
         square_numbers.append(i**2)
     #print(square_numbers)
+    
     if pt.prime_test_func(x) == 1:
     
         for sq in square_numbers:
             y = x - sq
+            
             #print(y)
             if y in square_numbers:
                 print(f'{x} = {y} + {sq}')
+                print(f'Test logic for sum: {x==y+sq}')
+                print(f'Test logic for prime: {(x-1)%4==0}')
                 square_numbers.remove(sq)
             else:
                 pass
